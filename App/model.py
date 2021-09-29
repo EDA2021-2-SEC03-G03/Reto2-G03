9a19f35fc1b789d2e5aad92169350cb0d8a735dc
Maria Alejandra Estrada <m.estradag@uniandes.edu.co>
2021-09-28 20:04:40 -0500

    Primera Entrega - Reto 2

--- App/model.py
+++ App/model.py
@@ -29,10 +29,6 @@ import config as cf
 from DISClib.ADT import list as lt
 from DISClib.ADT import map as mp
 from DISClib.DataStructures import mapentry as me
-from DISClib.Algorithms.Sorting import mergesort
-from DISClib.Algorithms.Sorting import shellsort
-from DISClib.Algorithms.Sorting import quicksort
-from DISClib.Algorithms.Sorting import insertionsort
 assert cf
 import time
 from datetime import date
